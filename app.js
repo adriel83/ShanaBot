e988fe2b31de02e42e80d85034bb2cfe50cb543d
adriel83 <37738709+adriel83@users.noreply.github.com>
2018-03-24 10:53:15 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const client = new Discord.Client();
 const settings = require('./settings.json');
 
 client.on('ready',() => {
-	console.log('I\'m Online\nI\'m Online');
+	console.log('Pronta para salvar o dia');
 });
 
 var prefix = "-"
 client.on('message', message => {
 	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'fon')) {
-		message.channel.sendMessage('fon');
+	if (message.content.startsWith('Vc me ama?')) {
+		message.channel.sendMessage('Claro, Baka');
 	}
 });
 
