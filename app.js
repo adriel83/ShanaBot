34a15639427ebfdc1165198858afc2c5205fe24c
adriel83 <37738709+adriel83@users.noreply.github.com>
2018-03-26 16:28:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ client.on('ready',() => {
 	console.log('Pronta para salvar o dia');
 	client.user.setPresence({ status: 'online', game: { name: 'o Yuuji da sacada' } });
 });
+cliente.on('message', message => {
+  if (&& message.content === 'tocar'){
+  message.member.voiceChannel.join().then(connection =>
+  {
+     const dispatcher = connection.playFile('home/dragonadriel/1.mp3');
+     dispatcher.on("end", end => {
+       voiceChannel.leave();
+       });
+   }).catch(err => console.log(err));
+  }
+});
 client.on('message', message => {
   if (message.content === 'avatar') {
     message.channel.send(message.author.avatarURL);
