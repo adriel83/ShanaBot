fde135b2d10b00ab55f18dacf8646d819321e3c5
adriel83 <37738709+adriel83@users.noreply.github.com>
2018-03-27 09:01:26 -0300

    Update cc.js

--- cc.js
+++ cc.js
@@ -9,7 +9,7 @@ bot.on('message', message => {
   var voiceChannel = message.member.voiceChannel;
   voiceChannel.join().then(connection =>
   {
-     const dispatcher = connection.playFile('./Audio/gab.mp3');
+     const dispatcher = connection.playFile('home/dragonadriel/1.mp3');
      dispatcher.on("end", end => {
        voiceChannel.leave();
        });
