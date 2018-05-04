69066deaefad63453fa0bdcb6cc72662acf0880e
adriel83 <37738709+adriel83@users.noreply.github.com>
2018-05-03 23:04:21 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ client.on('ready',() => {
 	client.user.setPresence({ status: 'online', game: { name: 'o Yuuji da sacada' } });
 });
 client.on('message', message => {
-  if (message.content === 'avatar') {
+  if (message.content.ignoreCase === 'avatar') {
     message.channel.send('Seu avatar '+ message.author.avatarURL);
 }
 });
@@ -23,18 +23,8 @@ client.on('message', message => {
 	    broadcast.playStream(stream);
 	    const dispatcher = connection.playBroadcast(broadcast);
 	    console.log('Tocando')
-	  })	
-}
-});
-client.on('message', message => {
-	if(message.content== 'tocar 2')
-	channel.join()
-	  .then(connection => {
-	    const stream = ytdl('https://www.youtube.com/watch?v=vh5qg6baBY8', { filter : 'audioonly' });
-	    broadcast.playStream(stream);
-	    const dispatcher = connection.playBroadcast(broadcast);
 	  })
-		console.log('Tocando')
+}
 });
 client.on('message', message => {
 
@@ -42,11 +32,5 @@ client.on('message', message => {
 	const channel = message.member.voiceChannel;
 	channel.leave();
   }
-});
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith('Vc me ama?')) {
-		message.channel.sendMessage('Claro, Baka');
-	}
 });
 client.login(settings.token);
