4b27455e79f8b5cf5e701665f67e310a0aeec8c7
Adriel de Souza <37738709+adriel83@users.noreply.github.com>
2018-09-16 18:32:38 -0300

    Add files via upload
    Improved some of the logic.

--- app.js
+++ app.js
@@ -8,27 +8,39 @@ client.on('ready',() => {
 	console.log('Pronta para salvar o dia');
 	client.user.setPresence({ status: 'online', game: { name: 'o Yuuji da sacada' } });
 });
+var tocando = 0;
 client.on('message', message => {
   if (message.content === 'avatar') {
     message.channel.send('Seu avatar '+ message.author.avatarURL);
 }
 });
 client.on('message', message => {
-        const channel = message.member.voiceChannel;
-	if (message.content.startsWith('tocar')){
-	var link = message.content.slice('tocar'.length)
-	channel.join()
-	  .then(connection => {
-	    const stream = ytdl(link, { filter : 'audioonly' });
-	    broadcast.playStream(stream);
-	    const dispatcher = connection.playBroadcast(broadcast);
-	  })	
+if (message.content.startsWith('tocar')){
+	if (tocando === 0) {
+		const channel = message.member.voiceChannel;
+		if (!channel) return message.channel.send('Você não está em um canal de voz.');
+		var link = message.content.slice('tocar'.length)
+		channel.join()
+		.then(connection => {
+		const stream = ytdl(link, { filter : 'audioonly' });
+		broadcast.playStream(stream);
+		tocando = 1;
+		const dispatcher = connection.playBroadcast(broadcast);
+            /*if (connection.speaking = false) {
+                dispatcher.end();
+                tocando = 0;
+            }*/
+		})
+	}else if (tocando === 1) {
+		return message.channel.send('Já existe uma musica tocando');
+	}
 }
 });
 client.on('message', message => {
-
-		if(message.content== 'sair') {
+if(message.content== 'sair') {
 	const channel = message.member.voiceChannel;
+	broadcast.end();
+	tocando = 0;
 	channel.leave();
   }
 });
