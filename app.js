48d16d33f4e5782ea16d4258845204f3bf704af8
Adriel de Souza <37738709+adriel83@users.noreply.github.com>
2018-11-14 13:32:27 -0200

    Update app.js

--- app.js
+++ app.js
@@ -15,54 +15,38 @@ client.on('message', message => {
 }
 });
 client.on('message', message => {
-
-if (message.content.startsWith('tocar')){
-
-	if (tocando === 0) {
-
-		const channel = message.member.voiceChannel;
-
-		if (!channel) return message.channel.send('Você não está em um canal de voz.');
-
-		var link = message.content.slice('tocar'.length)
-
-		channel.join()
-
-		.then(connection => {
-
-		const stream = ytdl(link, { filter : 'audioonly' });
-
-		broadcast.playStream(stream);
-
-		const dispatcher = connection.playBroadcast(broadcast);
-
-/*if (connection.speaking = false) {
-
-dispatcher.end();
-
-tocando = 0;
-
-}*/
-
-		})
-
-	}else if (tocando === 1) {
-
-		return message.channel.send('Já existe uma musica tocando');
-
-	}
-
-}
-
+    if (message.content.startsWith('tocar')){
+        const channel = message.member.voiceChannel;
+        if (!channel) return message.channel.send('Você não está em um canal de voz.');
+        const permissions = voiceChannel.permissionsFor(msg.client.user);
+        if(!permissions.has('CONNECT')){
+            return message.channel.send('Não posso entrar nesse canal.');
+        }
+        if(!permissions.has('SPEAK')){
+            return message.channel.send('Não posso falar nesse canal.');
+        }
+        var link = message.content.slice('tocar'.length)
+        channel.join()
+            .then(connection => {
+                const stream = ytdl(link, { filter : 'audioonly' });
+                broadcast.playStream(stream);
+                const dispatcher = connection.playBroadcast(broadcast);
+                dispatcher.on('end',reason => {
+                    channel.leave();
+                })
+            })
+    }
 });
-
 
 client.on('message', message => {
 if(message.content== 'sair') {
 	const channel = message.member.voiceChannel;
 	broadcast.end();
-	tocando = 0;
-	channel.leave();
+	if (channel){
+        channel.leave();
+    }else{
+        return message.channel.send('Não estou em um canal de voz.');
+    }
   }
 });
 client.login(settings.token);
