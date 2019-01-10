63f08c7153b842a89d711d5974556055cf0c825a
Adriel de Souza <adriel.2000@hotmail.com>
2019-01-10 14:14:12 -0200

    é isso

--- app.js
+++ app.js
@@ -2,51 +2,61 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
 const ytdl = require('ytdl-core');
-const streamOptions = { seek: 0, volume: 1 };
-const broadcast = client.createVoiceBroadcast();
-client.on('ready',() => {
-	console.log('Pronta para salvar o dia');
-	client.user.setPresence({ status: 'online', game: { name: 'o Yuuji da sacada' } });
-});
-var tocando = 0;
-client.on('message', message => {
-  if (message.content === 'avatar') {
-    message.channel.send('Seu avatar '+ message.author.avatarURL);
-}
-});
-client.on('message', message => {
-    if (message.content.startsWith('tocar')){
+
+//Bom, ela conecta, pega o url mas não toca e não dá erro.
+
+let fila = {};
+
+const commands = {
+    'tocar' :(message) =>{
+        if (fila === undefined) return message.channel.send(`Coloque uma música na fila.`);
+        if (fila.playing) return message.channel.send('Já estou tocando.');
+        if (!message.guild.voiceConnection) return commands.join(message).then(() => commands.play(message));
+        let dispatcher;
+        fila.playing = true;
+
+        function tocar(song){
+            message.channel.send(`Tocando:*${song.title}`);
+            dispatcher = message.guild.voiceConnection.playStream(ytdl(song.url, { audioonly: true }));
+        }
+    },
+    'entrar' : (message) =>{
         const channel = message.member.voiceChannel;
-        if (!channel) return message.channel.send('Você não está em um canal de voz.');
-        const permissions = channel.permissionsFor(message.client.user);
-        if(!permissions.has('CONNECT')){
-            return message.channel.send('Não posso entrar nesse canal.');
-        }
-        if(!permissions.has('SPEAK')){
-            return message.channel.send('Não posso falar nesse canal.');
-        }
-        var link = message.content.slice('tocar'.length)
-        channel.join()
-            .then(connection => {
-                const stream = ytdl(link, { filter : 'audioonly' });
-                broadcast.playStream(stream);
-                const dispatcher = connection.playBroadcast(broadcast);
-                dispatcher.on('end',reason => {
-                    channel.leave();
-                })
-            })
-    }
-});
+        if (!channel || channel.type !== 'voice') return message.reply('Não posso entrar nesse canal.');
+        channel.join();
+    },
+    'sair' : (message) =>{
+        const channel = message.member.voiceChannel;
+        if (!channel || channel.type !== 'voice') return message.reply('Não estou em um canal de voz.');
+        channel.leave();
+    },
+    'fila': (message) => {
+        if (fila === undefined) return message.channel.send(`Adicione uma musica á fila.`);
+        let tosend = [];
+        fila.songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.url} - Pedido por: ${song.requester}`);});
+        message.channel.send(`\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
+        // message.channel.send(tosend.length);
+
+    },
+    'adicionar': (message) => {
+        let url = message.content.split(' ')[1];
+        //if (url == '' || url === undefined) return msg.channel.sendMessage();
+        ytdl.getInfo(url, (err, info) => {
+            if(err) return message.channel.send('Link Inválido: ' + err);
+            fila.playing = false, fila.songs = [];
+            fila.songs.push({url: url, title: info.title, requester: message.author.username});
+            message.channel.send(`**${info.title}** adicionado á fila`);
+        });
+    },
+}
 
 client.on('message', message => {
-if(message.content== 'sair') {
-	const channel = message.member.voiceChannel;
-	broadcast.end();
-	if (channel){
-        channel.leave();
-    }else{
-        return message.channel.send('Não estou em um canal de voz.');
-    }
-  }
+    if (commands.hasOwnProperty(message.content.toLowerCase().split(' ')[0])) commands[message.content.toLowerCase().split(' ')[0]](message);
 });
+
+client.on('ready',() => {
+    console.log('Pronta para salvar o dia');
+    client.user.setPresence({ status: 'online', game: { name: 'o Yuuji da sacada' } });
+});
+
 client.login(settings.token);
