a010536da0b443720ffdf3ec1b0863060c96a15e
Adriel de Souza <adriel.2000@hotmail.com>
2019-08-09 14:41:24 -0300

    O ONIBUS VAI PASSAR COMITA LOGO ISSO

--- comandos/pesquisaPersonagem.js
+++ comandos/pesquisaPersonagem.js
@@ -1,10 +1,24 @@
 const Discord = require('discord.js');
 const Jikan = require('jikan-node');
 const mal = new Jikan();
-
+function criarDescricao (personagens){
+    var anime;
+    var nome;
+    let c;
+    for (i = 0; i<personagens.length && i < 10; i++){
+        if(personagens[i] !== undefined){
+            nome = personagens[i].name;
+            anime = personagens[i].anime[0].name;
+        }
+        if(i===0){
+            c = `${i+1}: Nome: ${nome} \n Anime: ${anime} \n \n`;
+        }
+        c += `${i+1}: Nome: ${nome} \n Anime: ${anime} \n \n`;
+    }
+    return c;
+}
 //TODO: Implementar um coletor de reação na mensagem que o bot envia para poder determinar qual personagem pesquisar no MAL
 //TODO: Implementar uma maneira de preencher o embed dinamicamente, com no máximo 5 personagem(talvez fazer paginação também).
-
 module.exports = {
     name: 'pesquisarp',
     description: 'Pesquisa um personagem no MAL.!',
@@ -18,35 +32,24 @@ module.exports = {
         // message.channel.send("O "+ nome + " ama muito a "+ nomeWaifu);
         mal.search('character', nomePersonagem, 1).then((result) =>{
             personagem = result.results[0];
-
             personagens = result.results;
+            var animePersonagens = [];
             for (i = 0; i < personagens.length; i++){
-                personagens[i].anime.sort((a,b) => (a.mal_id > b.mal_id) ? 1 : ((b.mal_id> a.mal_id) ? -1 : 0))
+                personagens[i].anime.sort((a,b) => (a.mal_id > b.mal_id) ? 1 : ((b.mal_id> a.mal_id) ? -1 : 0));
             }
-            // console.log(animePersonagens);
-            console.log(personagens[0].anime[0]);
-            console.log(personagens[1].anime[0]);
-            console.log(personagens[2].anime[0]);
-            console.log(personagens[3].anime[0]);
-
-            // console.log(personagens[0].anime);
-
             const embedSelecao = new Discord.RichEmbed()
-                .addField(`1  ${personagens[0].name}`, personagens[0].anime[0].name, false)
-                .addField(`2  ${personagens[1].name}`, personagens[1].anime[0].name, false)
-                .addField(`3  ${personagens[2].name}`, personagens[2].anime[0].name, false)
-                .addField(`4  ${personagens[3].name}`, personagens[3].anime[0].name, false)
-                .addField(`4  ${personagens[4].name}`, personagens[4].anime[0].name, false)
+                .setTitle("Resultados")
+                .setDescription(criarDescricao(personagens))
                 .setTimestamp();
-
             message.channel.send(embedSelecao);
-            // message.react();
 
+
+            //React de mensagem
             //:one:, :two: ,:three: ,:four:
-            const filter = (reaction, user) => reaction.emoji.name === ":one:" && user.id === message.author.id;
-            message.awaitReactions(filter, { time: 15000 })
-                .then(collected => console.log(`Collected ${collected.size} reactions`))
-                .catch(console.error);
+            // const filter = (reaction, user) => reaction.emoji.name === ":one:" && user.id === message.author.id;
+            // message.awaitReactions(filter, { time: 15000 })
+            //     .then(collected => console.log(`Collected ${collected.size} reactions`))
+            //     .catch(console.error);
 
             mal.findCharacter(personagem.mal_id, '').then((result) =>{
                 // console.log(result);
