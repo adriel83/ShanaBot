3782a93641e28d14d28273f4ab04e3803c1fa226
Adriel de Souza <adriel.2000@hotmail.com>
2019-08-23 11:32:47 -0300

    Correção de algumas coisas no código.

--- comandos/pesquisaPersonagem.js
+++ comandos/pesquisaPersonagem.js
@@ -18,13 +18,9 @@ function criarDescricao (personagens){
             nomeManga = manga[0].name;
             c += `${i+1}: **${nome}** \n ${nomeManga} \n \n`;
         }
-        // console.log(anime);
-        // anime = personagens[i].anime[1].name;
     }
     return c;
 }
-//TODO: Implementar um coletor de reação na mensagem que o bot envia para poder determinar qual personagem pesquisar no MAL
-//TODO: Implementar uma maneira de preencher o embed dinamicamente, com no máximo 5 personagem(talvez fazer paginação também).
 module.exports = {
     name: 'pesquisarp',
     description: 'Pesquisa um personagem no MAL.!',
@@ -35,7 +31,6 @@ module.exports = {
     execute(message, args) {
         nome = message.author.username;
         nomePersonagem = args;
-        // message.channel.send("O "+ nome + " ama muito a "+ nomeWaifu);
         mal.search('character', nomePersonagem, 1).then((result) =>{
             personagens = result.results;
             let numerosFiltro = [];
@@ -44,7 +39,7 @@ module.exports = {
                 console.log(numerosFiltro);
             }
             for (i = 0; i < personagens.length; i++){
-                personagens[i].anime.sort((a,b) => (a.mal_id > b.mal_id) ? 1 : ((b.mal_id> a.mal_id) ? -1 : 0));
+                personagens[i].anime.sort((a,b) => (a.mal_id > b.mal_id) ? 1 : ((b.mal_id > a.mal_id) ? -1 : 0));
             }
             const embedSelecao = new Discord.RichEmbed()
                 .setTitle("Resultados")
@@ -53,7 +48,6 @@ module.exports = {
             message.channel.send(embedSelecao);
             message.channel.send("Escolha o personagem desejado enviando o numero correspondente").then(() => {
                 let i;
-                // const filter =  m => numerosFiltro.includes(m.content);
                 message.channel.awaitMessages(response => {
                         for(i = 0; i<numerosFiltro.length; i++){
                             if(response.content === numerosFiltro[i]){
@@ -80,7 +74,6 @@ module.exports = {
                                 .addField('Anime origem', personagem.animeography[0].name +'\n'+ personagem.animeography[0].url, true)
                                 .addField('Dublador(a)', personagem.voice_actors[0].name , true)
                                 .setURL(personagem.animeography[0].url)
-                                // .addBlankField()
                                 .setTimestamp();
                             message.channel.send(embedWaifu);
                         });
@@ -89,19 +82,9 @@ module.exports = {
                         message.channel.send('O tempo acabou.');
                     });
             });
-
-
-            //React de mensagem
-            //:one:, :two: ,:three: ,:four:
-            // const filter = (reaction, user) => reaction.emoji.name === ":one:" && user.id === message.author.id;
-            // message.awaitReactions(filter, { time: 15000 })
-            //     .then(collected => console.log(`Collected ${collected.size} reactions`))
-            //     .catch(console.error);
-
         }).catch((response) => {
             console.log(response);
             message.channel.send(`Nao encontrei nenhum personagem chamado ${nomePersonagem}`);
         });
-
     },
 };
